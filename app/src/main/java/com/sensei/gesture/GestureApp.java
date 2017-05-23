988b37b22086c09b7d3a0f6fc99fc78053660fe9
Salar Hosseini <salar77h@gmail.com>
2017-05-23 15:55:16 -0400

    comment

--- app/src/main/java/com/sensei/gesture/GestureApp.java
+++ app/src/main/java/com/sensei/gesture/GestureApp.java
@@ -9,5 +9,6 @@ public class GestureApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gesture_app);
+        //ANUP OMG HI
     }
 }
