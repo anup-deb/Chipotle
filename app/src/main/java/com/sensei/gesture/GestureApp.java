a699256f4328c016daddb70ad67c6a7f9bd7fc49
Onik Deb <onikdeb@Oniks-MacBook-Pro.local>
2017-05-23 15:03:38 -0400

    Initial commit

---
+++ app/src/main/java/com/sensei/gesture/GestureApp.java
@@ -0,0 +1,13 @@
+package com.sensei.gesture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GestureApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gesture_app);
+    }
+}
