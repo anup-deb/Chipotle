fb6c7b13d4b94fd5984c5d0d560e97b857077969
Salar Hosseini <salar77h@gmail.com>
2017-06-03 17:44:01 -0400

    fixing conflict

--- app/src/main/java/com/sensei/gesture/actions/Action.java
+++ app/src/main/java/com/sensei/gesture/actions/Action.java
@@ -3,4 +3,14 @@ package com.sensei.gesture.actions;
 import android.content.Context;
 
 public class Action {
+
+    public static void doAction (Context context, String actionKey) {
+        if (actionKey.equals ("whatsapp")) {
+            AppAction.openApp (context, "com.whatsapp");
+        }
+        else {
+            //TODO: implement other action calls
+        }
+    }
+
 }
